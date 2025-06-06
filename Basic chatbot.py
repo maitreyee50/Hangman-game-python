5543dafd53fab2b49e6980016e7c9125fe5c21ee
maitreyee50 <roises14@gmail.com>
2025-06-06 20:03:57 +0530

    basic chatboot push

---
+++ Basic chatbot.py
@@ -0,0 +1,17 @@
+print("Chatbot: Hello! Type something to start chatting. Type 'bye' to exit.")
+
+while True:
+    user_input = input("You: ").lower()
+
+    if user_input == "hello":
+        print("Chatbot: Hi!")
+    elif user_input == "Good Morning":
+        print("Chatbot: Good Morning!")
+    elif user_input == "how are you":
+        print("Chatbot: I'm fine, thanks!")
+    elif user_input == "bye":
+        print("Chatbot: Goodbye!")
+        break
+    else:
+        print("Chatbot: I don't understand that.")
+
